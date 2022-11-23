c79b97ebafa5e785c374fbd9216ca2fae34138a3
Jeff Pinegar <jeffpinegar1@gmail.com>
2022-11-23 15:34:15 -0500

    Analysis Complete

---
+++ EmployeeSQL/PH2_Create_tables_and_keys.sql
@@ -0,0 +1,94 @@
+﻿-- Jeff Pinegar
+-- Nov 30, 2022
+-- SQL Challange
+
+-- Code to create the tables and keys
+
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/jvsBOg
+
+-- *****************************************************************
+-- I modified the  export to split into two schemas 'hr' and 'dept'
+-- ****************************************************************
+
+
+-- hr schemas employee table
+CREATE TABLE hr.emp (
+    "emp_id" int   NOT NULL,
+    "emp_title_id" character(5)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(20)   NOT NULL,
+    "last_name" varchar(30)   NOT NULL,
+    "sex" character(1)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_emp" PRIMARY KEY (
+        "emp_id"
+     )
+);
+
+
+-- hr schemas employee salaries
+CREATE TABLE hr.salaries (
+    "emp_id" int   NOT NULL,
+    "salaries" money   NOT NULL
+);
+
+-- hr schemas titles table
+CREATE TABLE hr.titles (
+    "title_id" character(5)   NOT NULL,
+    "title" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+
+-- dept schemas dept_emp table
+CREATE TABLE dept.dept_emp (
+    "emp_id" int   NOT NULL,
+    "dept_id" char(4)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_id","dept_id"
+     )
+);
+
+
+-- dept schemas dept_mgr table
+CREATE TABLE dept.dept_mgr (
+    "dept_id" char(4)   NOT NULL,
+    "emp_id" int   NOT NULL,
+    CONSTRAINT "pk_dept_mgr" PRIMARY KEY (
+        "dept_id","emp_id"
+     )
+);
+
+
+-- dept schemas depts table
+CREATE TABLE dept.depts (
+    "dept_id" char(4)   NOT NULL,
+    "dept_name" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_depts" PRIMARY KEY (
+        "dept_id"
+     )
+);
+
+
+-- Alter tables to add forign keys
+ALTER TABLE hr.emp ADD CONSTRAINT "fk_emp_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES hr.titles ("title_id");
+
+ALTER TABLE hr.salaries ADD CONSTRAINT "fk_salaries_emp_id" FOREIGN KEY("emp_id")
+REFERENCES hr.emp ("emp_id");
+
+ALTER TABLE dept.dept_emp ADD CONSTRAINT "fk_dept_emp_emp_id" FOREIGN KEY("emp_id")
+REFERENCES hr.emp ("emp_id");
+
+ALTER TABLE dept.dept_emp ADD CONSTRAINT "fk_dept_emp_dept_id" FOREIGN KEY("dept_id")
+REFERENCES dept.depts ("dept_id");
+
+ALTER TABLE dept.dept_mgr ADD CONSTRAINT "fk_dept_mgr_dept_id" FOREIGN KEY("dept_id")
+REFERENCES dept.depts ("dept_id");
+
+ALTER TABLE dept.dept_mgr ADD CONSTRAINT "fk_dept_mgr_emp_id" FOREIGN KEY("emp_id")
+REFERENCES dept.emp ("emp_id");
+
---
+++ EmployeeSQL/PH2_Homework Solutions.sql
@@ -0,0 +1,80 @@
+
+-- Jeff Pinegar
+-- Nov 30, 2022
+-- SQL Challange
+
+-- *********************
+-- Data Analysis
+-- *********************
+
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+select emp.emp_id, emp.last_name, emp.first_name, emp.sex, salaries.salaries
+	from hr.emp join hr.salaries
+	on emp.emp_id = salaries.emp_id
+	order by emp.emp_id
+-- REsults in: Q1_Employee_salaries.csv
+
+
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+select emp.emp_id, emp.last_name, emp.first_name, emp.hire_date
+	from hr.emp
+	where emp.hire_date > '1986-01-10' and emp.hire_date < '1986-12-31'
+	order by emp.emp_id
+-- Results in:  Q2_Employees_hired_in_1986.csv
+
+
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+select depts.dept_id
+	, depts.dept_name
+	, dept_mgr.emp_id as Manager_Emp_id
+	, emp.last_name as Manager_Last_Name
+	, emp.first_name as Manager_First_Name
+	from dept.dept_mgr
+	join dept.depts on depts.dept_id = dept_mgr.dept_id
+	join hr.emp on emp.emp_id = dept_mgr.emp_id
+-- Results in:  Q3_Department Managers.csv
+
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+select emp.emp_id, emp.last_name, emp.first_name, depts.dept_name
+	From dept.dept_emp 
+	join hr.emp on emp.emp_id = dept_emp.emp_id
+	join dept.depts on dept_emp.dept_id = depts.dept_id	
+	order by emp.emp_id
+-- Results in:  Q4_Employee_dept_list.cvs
+
+
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select emp.emp_id, emp.first_name, emp.last_name, emp.sex
+	from hr.emp
+	where emp.first_name = 'Hercules' and substring(emp.last_name,1,1) = 'B'
+	order by emp.emp_id
+-- Results in:  Q5_Employee_Hercules_B.cvs
+
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select emp.emp_id, emp.last_name, emp.first_name, depts.dept_name
+	From dept.dept_emp 
+	join hr.emp on emp.emp_id = dept_emp.emp_id
+	join dept.depts on dept_emp.dept_id = depts.dept_id
+	where depts.dept_name = 'Sales'
+	order by emp.emp_id
+-- Results in:  Q6_Employee_Sales.cvs
+
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+select emp.emp_id, emp.last_name, emp.first_name, depts.dept_name
+	From dept.dept_emp 
+	join hr.emp on emp.emp_id = dept_emp.emp_id
+	join dept.depts on dept_emp.dept_id = depts.dept_id
+	where depts.dept_name = 'Sales' or depts.dept_name = 'Development'
+	order by  depts.dept_name, emp.emp_id 
+-- Results in: Q7_Employee_Dev_or_Sales.csv
+
+
+-- 8. List the frequency count of employee last names (i.e., how many employees share each last name) in descending order.
+select emp.last_name, count(emp_id)
+	from hr.emp
+	Group by emp.last_name
+	order by count DESC
+-- Results in:  Q8_Last_name_freq_count.csv
